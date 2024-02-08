cdd32410343ce2f34f2dce19e7dad819b0d7f2c8
grouls <roulstone.gary@gmail.com>
2024-02-08 17:31:35 +0000

    make nav responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,42 @@
+import { TbShoppingBag } from "react-icons/tb";
 import NikeLogo from "../assets/nike-logo.svg?react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { ROUTES } from "../config";
+import { useState } from "react";
 
 const Nav = () => {
+  const [isMobileMenuShow, setIsMobileMenuShown] = useState(false);
   return (
     <nav className="flex flex-wrap justify-between items-center">
       <a href="">
         <NikeLogo className="h-20 w-20" />
       </a>
-      <button className="p-2 focus:ring-2 focus:ring-gray-200 rounded-lg hover:bg-gray-100">
+      <button
+        onClick={() => setIsMobileMenuShown(!isMobileMenuShow)}
+        className="p-2 focus:ring-2 focus:ring-gray-200 rounded-lg hover:bg-gray-100 lg:hidden"
+      >
         <RxHamburgerMenu size={25} />
       </button>
-      <div className="w-full">
-        <ul className="bg-gray-50 text-lg border border-gray-100 rounded-lg p-4">
+      <div
+        className={`${!isMobileMenuShow && "hidden"} w-full lg:w-auto lg:block`}
+      >
+        <ul className="flex flex-col lg:flex-row bg-gray-50 text-lg border border-gray-100 rounded-lg p-4 lg:bg-transparent lg:border-none lg:space-x-8">
           {ROUTES.map((route, i) => {
             return (
               <li
                 key={route}
-                className={`py-2 px-3 cursor-pointer rounded ${i === 0 ? "bg-blue-500 text-white" : "hover:bg-gray-100"}`}
+                className={`py-2 px-3 cursor-pointer rounded ${i === 0 ? "bg-blue-500 text-white lg:bg-transparent lg:text-blue-500" : "hover:bg-gray-100"}`}
               >
                 {route}
               </li>
             );
           })}
         </ul>
+      </div>
+      <div className="fixed left-4 bottom-4 lg:static">
+        <div className="flex-center h-12 w-12 rounded-full bg-white shadow-md">
+          <TbShoppingBag />
+        </div>
       </div>
     </nav>
   );
